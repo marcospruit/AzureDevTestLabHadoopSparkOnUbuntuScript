2c19539f8ecb740d9858801b55830b0083257570
Rene Bremer <rebremer@microsoft.com>
2018-09-12 20:26:23 +0200

    Remove test

--- Artifacts/exportvartest/exportvartest.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# update ubuntu
-sudo apt-get update -y
-
-# set 
-cd /etc/profile.d
-sudo wget https://raw.githubusercontent.com/rebremer/AzureDevTestLabHadoopSparkOnUbuntuScript/master/exportvartest/setenvvar.sh
