1a26b91b10e848ca6d39e931805a25b49df232ac
Rene Bremer <rebremer@microsoft.com>
2018-09-15 00:03:26 +0200

    added HadoopHive, added 18.04 in description

---
+++ Artifacts/HadoopHive/readme.md.txt
@@ -0,0 +1,3 @@
+# run the following command if you start hive for the first time:
+
+$HIVE_HOME/bin/schematool -initSchema -dbType derby
---
+++ Artifacts/HadoopHive/setenvvarHadoopHive.sh
@@ -0,0 +1,5 @@
+export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64/jre/
+export HADOOP_HOME=/usr/local/hadoop
+export PATH=$HADOOP_HOME/bin:$PATH
+export HIVE_HOME=/usr/local/hive
+export PATH=$HIVE_HOME/bin:$PATH
