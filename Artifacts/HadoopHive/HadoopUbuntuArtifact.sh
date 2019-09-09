a649027570c71bdc878fcaedaef5ed7170f852b0
Marco <marco@spru.it>
2019-09-09 17:02:08 +0200

    Update and rename HadoopHiveUbuntuArtifact.sh to HadoopUbuntuArtifact.sh

---
+++ Artifacts/HadoopHive/HadoopUbuntuArtifact.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+# update ubuntu
+sudo apt-get update -y
+
+# install ssh
+sudo apt-get install ssh -y
+sudo apt-get install rsync -y
+
+# install java
+sudo apt-get install openjdk-8-jre -y
+export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64/jre/
+
+# download hadoop
+# wget http://apache.40b.nl/hadoop/common/hadoop-3.1.1/hadoop-3.1.1.tar.gz
+wget https://www.apache.org/dyn/closer.cgi/hadoop/common/hadoop-3.2.0/hadoop-3.2.0.tar.gz
+tar -xvf hadoop-3.2.0.tar.gz
+
+# move hadoop to usr directory
+sudo mv hadoop-3.2.0 /usr/local/hadoop
+export HADOOP_HOME=/usr/local/hadoop
+export PATH=$HADOOP_HOME/bin:$PATH
+
+# test 1: version
+hadoop version
+
+# test 2: HDFS
+hdfs classpath
+
+# finally, set environment variables in /etc/profile.d such that user does not have to execute the export commands
+cd /etc/profile.d
+sudo wget https://raw.githubusercontent.com/marcospruit/AzureDevTestLabHadoopSparkOnUbuntuScript/master/Artifacts/HadoopHive/setenvvarHadoopHive.sh
